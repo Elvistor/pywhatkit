6275d8c406ddec41d3696887a78f8eb1ccbc1b85
elvistor <pupuma147@gmail.com>
2021-05-29 19:06:24 -0300

    Commit inicial

---
+++ texto.py
@@ -0,0 +1,4 @@
+import pywhatkit as kit
+"""Transforma un string en una texto escrito a mano"""
+#Ingrese el texto que desea transformar
+kit.text_to_handwriting(input("Ingrese un texto: "), rgb=[0,0,138])
---
+++ texto_ascii.py
@@ -0,0 +1,6 @@
+#pip install pywhatkit
+import pywhatkit as kit
+
+"""Transforma una imagen y la representa en texto"""
+#Ingresar el path de la imagen como argumento
+kit.image_to_ascii_art("./img/mate.jpeg")
